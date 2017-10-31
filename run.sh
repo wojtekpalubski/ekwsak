c8f8ccf8b2165f3a4514828cba6b00ca6a29a095
wojtekpalubski <wojtek468@wp.pl>
2017-10-31 14:31:55 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,3 +2,6 @@ ansible --version
 ansible all -m ping
 ansible-playbook ekwsak.yml --list-hosts
 ansible-playbook ekwsak.yml --syntax-check
+cd /home/tstsak/sak
+sudo docker-compose pull
+sudo docker-compose up
